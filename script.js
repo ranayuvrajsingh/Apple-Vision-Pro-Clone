30b1c55a72105f28b7805ddd094a613f902394f0
ranayuvrajsingh <ranayuvrajsingh0000@gmail.com>
2023-10-02 10:36:20 +0530

    initial

--- script.js
+++ script.js
@@ -71,3 +71,31 @@ gsap.to("#page-bottom", {
   },
   opacity: 0,
 });
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: `#page1`,
+    start: `top top`,
+    scrub: 1,
+    scroller: `#main`,
+    pin: true,
+  },
+});
+
+tl.to("#page1>h1", {
+  top: `-50%`,
+});
+
+var tl1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: `#page2`,
+    start: `top top`,
+    scrub: 1,
+    scroller: `#main`,
+    pin: true,
+  },
+});
+
+tl1.to("#page2>h1", {
+  top: `-50%`,
+});
